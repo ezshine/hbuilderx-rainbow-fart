1dc0c69f02db305e2e3a948163725e710400871a
ezshine <ezshine@live.cn>
2020-07-01 22:26:43 +0800

    add virtual wife,amazing rainbowfart wife

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 /*
 编程伴侣（HBuilderX插件）
-版本：0.0.6
+版本：0.1.0
 作者：ezshine
 
 此插件的创意来源是VSCode的彩虹屁插件(感谢感谢)，兼容彩虹屁插件的语音包。
@@ -9,23 +9,68 @@
 */
 
 const hx = require("hbuilderx");
+
 const {
 	exec
 } = require('child_process');
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
+
+//用于和live2dPlayer进程通讯的信使
+const messenger = require('messenger');
+const server = messenger.createListener(32719);
+const client = messenger.createSpeaker(32718);
+
+//监听HBuilder Node进程的退出事件，退出时通知live2dPlayer
+const process = require('process');
+process.on('exit', (code) => {
+	client.shout('hbuilderExit');
+});
+
+let input_analysis;
+let coding_start;
 //该方法将在插件激活的时候调用
 function activate(context) {
 	console.log("好戏开始了：" + Date.now());
 	console.log("当前系统为" + os.type);
+	
+	//初始化命中历史
+	coding_start=Date.now();
+	input_analysis={};
+	
+	//注册命令
+	setupCommands();
 
 	//获得插件配置
 	const pluginConfig = hx.workspace.getConfiguration("codinglover");
 
 	//根据当前系统是window还是macos选择默认的音频播放器
-	const mp3Player = (os.type == "Darwin" ? "afplay" : path.join(__dirname, "mp3player", "cmdmp3new", "cmdmp3.exe"));
+	const mp3Player = (os.type == "Darwin" ? "afplay" : path.join(__dirname, "players","mp3player.exe"));
 
+	//是否禁用虚拟老婆
+	const disableLive2d = pluginConfig.get("codinglover.disableLive2d",false);
+	//虚拟老婆名称
+	const live2dPackageName = pluginConfig.get("live2dPackageName", "liang") || "liang";
+	const lpPath=path.join(__dirname, "live2dpackages", live2dPackageName);
+	server.on('wifeLoaded', function(m, data){
+		console.log("老婆起床啦："+lpPath);
+		client.shout('initModel', {model: lpPath+"/model.json"});
+	});
+	//唤醒老婆容器
+	const live2dPlayer=path.join(__dirname, "players","live2dplayer."+(os.type == "Darwin" ? "app" : "exe"));
+	if(!disableLive2d){
+		if (fs.existsSync(live2dPlayer)) {
+			let cmd=(os.type == "Darwin" ? "open" : "start")+" "+live2dPlayer;
+			console.log("执行唤醒老婆的命令："+cmd);
+			exec(cmd);
+		}else{
+			showInformation('没有找到老婆容器：' + live2dPlayer);
+		}
+	}
+
+	//是否禁用彩虹屁
+	const disabledRainbowFart = pluginConfig.get("disabledRainbowFart",false);
 	//输入监测时间间隔
 	const inputDetectInterval = pluginConfig.get("inputDetectInterval", 5000) || 5000;
 	//语音包名称
@@ -56,18 +101,14 @@ function activate(context) {
 	//上次查询的时间戳，用户计算间隔
 	let pre = 0;
 	//当编辑器中正在输入的时候
-	let onDidChangeTextDocumentEventDispose = hx.workspace.onDidChangeTextDocument(function(event) {
-		let activeEditor = hx.window.getActiveTextEditor();
+	const onDidChangeTextDocumentEventDispose = hx.workspace.onDidChangeTextDocument(function(event) {
+		const activeEditor = hx.window.getActiveTextEditor();
 		activeEditor.then(function(editor) {
-			let linePromise = editor.document.lineFromPosition(editor.selection.active);
+			const linePromise = editor.document.lineFromPosition(editor.selection.active);
 			linePromise.then((line) => {
 				//当行字符超过50时不做处理
 				if (line.length > 50) return;
-				let now = Date.now();
-				if (now - pre > inputDetectInterval) {
-					pre = now;
-					parseLine(line.text.trim());
-				}
+				parseLine(line.text.trim());
 			});
 		});
 	});
@@ -75,14 +116,21 @@ function activate(context) {
 	//重头戏在这里，解析当前行内容并作出相应的反应
 	//todo 优化查询性能
 	function parseLine(str) {
-
+		
+		const now = new Date();
+		if (now - pre < inputDetectInterval) {
+			//当两次解析间隔小于输入监测间隔时取消本次处理
+			return;
+		}
+		//重设时间间隔起点
+		pre = now;
+		
 		//优先时间提醒
-		let now = Date.now();
 		//当上一次的提示不是时间提醒，并且距离上次提醒超过30分钟时才执行时间判断
 		if (voice_mark.indexOf("$") < 0 && now - time_alarm > 1800000) {
-			time_alarm = now;
-			let hour = new Date().getHours();
-			let minute = new Date().getMinutes();
+			time_alarm = now.getTime();
+			const hour = now.getHours();
+			const minute = now.getMinutes();
 			if (minute == 0 && voice_mark != "$time_each_hour") {
 				voice_mark = str = "$time_each_hour";
 			} else if (hour > 6 && hour <= 9 && voice_mark != "$time_morning") {
@@ -100,24 +148,35 @@ function activate(context) {
 
 		let voices = [];
 		hitkeyword: for (let i = vpContributes.length - 1; i >= 0; i--) {
-			let item = vpContributes[i];
-			let keywords = item.keywords;
+			const item = vpContributes[i];
+			const keywords = item.keywords;
 			for (let j = keywords.length - 1; j >= 0; j--) {
 				if (str.indexOf(keywords[j]) >= 0) {
-					voice_mark = keywords[j];
+					const keyword=keywords[j];
+					voice_mark = keyword;
 					voices = item.voices;
 					break hitkeyword;
 				}
 			}
 		}
 
-		//命中以及和上次命中结果不一样时才播放
+		//命中以及和上次命中结果不一样时才播放，加入彩虹屁开关
 		if (voices.length != 0 && last_voice_mark != voice_mark) {
-			console.log("播放音频：" + voice_mark);
-			last_voice_mark = voice_mark;
-			const cmd = mp3Player + " " + path.join(vpPath, voices[Math.floor(Math.random() * voices.length)]);
-			console.log(cmd);
-			exec(cmd);
+			addInputAnalysis(voice_mark,now);
+			if(!disabledRainbowFart){
+				console.log("播放音频：" + voice_mark);
+				showFace("");
+				last_voice_mark = voice_mark;
+				
+				//音频播放器命令路径，将空格转义
+				let playerpath=mp3Player.replace(/ /g, '\\ ');
+				let audiopath=path.join(vpPath, voices[Math.floor(Math.random() * voices.length)]);
+				audiopath=audiopath.replace(/ /g, '\\ ');
+				
+				const cmd = mp3Player + " " + audiopath;
+				console.log(cmd);
+				exec(cmd);
+			}
 		}
 	}
 }
@@ -126,13 +185,39 @@ function showInformation(msg) {
 	hx.window.showInformationMessage('<span style="color:#3366ff">编程伴侣</span><br>' + msg);
 }
 
+function showFace(facename){
+	// client.shout('a message came', {some: "123"});
+	// hx.window.showWarningMessage('<img src="'+__dirname+'/facepackages/default/9wnQ-hkahyhx9261954.gif" style="width:100px;height:100px;"/>');
+}
+
 function setStatusMsg(msg, autohide = 2000) {
 	hx.window.setStatusBarMessage('<span style="color:#3366ff">编程伴侣：</span>' + msg);
 }
 
+function setupCommands(){
+	
+	let disposable = hx.commands.registerTextEditorCommand('extension.showCodingAnalysis',(editor)=>{
+		let report_str="编程时长 "+(Date.now()-coding_start)/1000+"秒 <br>";
+		for(let item in input_analysis){
+			report_str+=item+" 总计："+input_analysis[item].times+"<br>";
+		}
+		showInformation(report_str);
+	});
+}
+
+function addInputAnalysis(keyword,time){
+	if(input_analysis[keyword]){
+		input_analysis[keyword].times+=1;
+	}else{
+		input_analysis[keyword]={
+			times:1
+		};
+	}
+}
+
 //该方法将在插件禁用的时候调用（目前是在插件卸载的时候触发）
 function deactivate() {
-
+	
 }
 module.exports = {
 	activate,
